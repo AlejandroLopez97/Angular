4b424e6de8f3d1203f7b490bd8d06377ddf57f60
AlejoOliveros <alejooliveros97@outlook.com>
2024-02-23 15:29:53 -0500

    Creacion del proyecto, manejo de eventos y signal

---
+++ src/app/pages/labs/labs.component.ts
@@ -0,0 +1,41 @@
+import { Component, signal } from '@angular/core';
+import { concatWith } from 'rxjs';
+
+@Component({
+  selector: 'app-labs',
+  standalone: true,
+  imports: [],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.css'
+})
+export class LabsComponent {
+  welcome = 'Hola';
+  tasks = [
+    'Instalar Angular CLI',
+    'Crear proyecto',
+    'Crear Componentes'
+  ];
+  nombre = signal('Alejandro');
+  edad = 26;
+  disabled = true;
+  img = 'https://w3schools.com/howto/img_avatar.png';
+
+  persona = {
+    nombre: 'Alejandro',
+    edad: 27,
+    avatar: 'https://w3schools.com/howto/img_avatar.png'
+  }
+
+  clickHandler(){
+    alert('Hola');
+  }
+
+  changerHander(event: Event){
+    console.log(event);
+  }
+
+  keydownHander(event: KeyboardEvent){
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
+  }
+}
